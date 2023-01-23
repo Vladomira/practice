9e168437f8f43afcfcea136eea0c4628c5c25d00
Julia_Vladomira <juliavladomira@gmail.com>
2023-01-23 13:37:29 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(40);
+
+    return (
+        <div className="App">
+            <div></div>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 5)}>
+                    count plus
+                </button>
+                <div>{count}</div>
+                <button onClick={() => setCount((count) => count - 5)}>
+                    count minus
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
